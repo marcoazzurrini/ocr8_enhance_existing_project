bb8ea68d8ece101a9bda2453b5e0a8b106ec773a
marcoazzurrini <marco.azzurrini.art@gmail.com>
2020-02-08 20:01:34 +0100

    fix adddItem typo at controller.js line 95

---
+++ mvc.js
@@ -0,0 +1,12 @@
+// Model view controller - software achitecture design
+// separates application functionality - promotes organized code
+
+// Model
+// Brain of the app, data related login, communicates with controller
+
+// View
+// It's the user interface, consists of HTML,CSS, Template engines when used (handlebars), can be passed dynamic values from controller.
+
+// Controller
+// Receive user input and processes requests, gets data from the model. Middle main
+// Model -> Controller -> View
